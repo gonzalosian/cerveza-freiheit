fc66511d2a5045c6d4539745d6e4e998d4a37d1f
Gonzalo Sian <gonzalosian@gmail.com>
2021-05-22 22:07:22 -0300

    Primer commit

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+// AOS - https://michalsnik.github.io/aos/
+AOS.init();
+
+// let menu = document.getElementById('menu');
+// let navigation = document.getElementById('navigation');
+
+// navigation.addEventListener('click', () => {
+//     menu.classList.toggle('menu-toggle');
+// })
+
+var menuIcon = document.getElementById('menu-icon');
+var menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+    } else {
+        menu.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click1');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click2');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click3');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click4');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click5');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
+
+var enlace = document.getElementById('click6');
+var menu2 = document.getElementById('menu');
+enlace.addEventListener('click', () => {
+    if (menu2.classList.contains('show')) {
+        menu2.classList.remove('show');
+    } else {
+        menu2.classList.add('show');
+    }
+});
